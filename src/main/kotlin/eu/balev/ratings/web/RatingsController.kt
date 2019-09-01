48df469541d6072d7904fc24bfac777d77daa635
Lachezar Balev <lachezar.balev@gmail.com>
2019-09-01 14:43:58 +0300

    just enable the cors stuff

--- src/main/kotlin/eu/balev/ratings/web/RatingsController.kt
+++ src/main/kotlin/eu/balev/ratings/web/RatingsController.kt
@@ -3,17 +3,20 @@ package eu.balev.ratings.web
 import eu.balev.ratings.model.Rating
 import eu.balev.ratings.repository.RatingRepository
 import eu.balev.ratings.service.RatingsService
+import org.slf4j.LoggerFactory
 import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.handler.annotation.MessageMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
+@CrossOrigin(origins = ["*"])
 @RestController
 class RatingsController(val ratingRepository: RatingRepository,
                         val ratingService: RatingsService) {
+
+    companion object {
+        private val LOGGER = LoggerFactory.getLogger(RatingsController::class.java)
+    }
 
     @MessageMapping("/ratings")
     @SendTo("/topic/ratings")
@@ -25,6 +28,9 @@ class RatingsController(val ratingRepository: RatingRepository,
 
     @PostMapping("/ratings")
     fun createRating(@Valid @RequestBody rating: Rating) {
+
+        LOGGER.debug("Received a rating, trying to save...");
+
         this.ratingRepository.save(rating);
         this.ratingService.sendRating(rating)
     }
