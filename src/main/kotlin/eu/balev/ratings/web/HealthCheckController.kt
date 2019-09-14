c1ecce8033116260f038344dfebb2d1bf3feec18
Lachezar Balev <lachezar.balev@gmail.com>
2019-09-14 12:37:34 +0300

    Add a healthcheck controller, configure the credentials and DB settings to come from an config map.

---
+++ src/main/kotlin/eu/balev/ratings/web/HealthCheckController.kt
@@ -0,0 +1,17 @@
+package eu.balev.ratings.web
+
+import org.springframework.http.ResponseEntity
+import org.springframework.http.ResponseEntity.ok
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(value = ["/admin/healthcheck"], produces = ["text/plain"])
+class HealthCheckController {
+
+    @GetMapping
+    fun healthCheck(): ResponseEntity<Void> {
+        return ok().build()
+    }
+}
