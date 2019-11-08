c6b66419b4107a0b0bb93db727fb8b12a0fe30fb
georgi.peychev <georgi.peychev@rewe-digital.com>
2019-11-08 19:38:17 +0200

    Added API for getting ratings.

--- src/main/kotlin/eu/balev/ratings/web/RatingsController.kt
+++ src/main/kotlin/eu/balev/ratings/web/RatingsController.kt
@@ -4,8 +4,6 @@ import eu.balev.ratings.model.Rating
 import eu.balev.ratings.repository.RatingRepository
 import eu.balev.ratings.service.RatingsService
 import org.slf4j.LoggerFactory
-import org.springframework.messaging.handler.annotation.SendTo
-import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
@@ -14,17 +12,22 @@ import javax.validation.Valid
 class RatingsController(val ratingRepository: RatingRepository,
                         val ratingService: RatingsService) {
 
-    companion object {
-        private val LOGGER = LoggerFactory.getLogger(RatingsController::class.java)
-    }
+  companion object {
+    private val LOGGER = LoggerFactory.getLogger(RatingsController::class.java)
+  }
 
-    @PostMapping("/ratings")
-    fun createRating(@Valid @RequestBody rating: Rating) {
+  @PostMapping("/ratings")
+  fun createRating(@Valid @RequestBody rating: Rating) {
 
-        LOGGER.debug("Received a rating, trying to save...");
+    LOGGER.debug("Received a rating, trying to save...");
 
-        this.ratingRepository.save(rating);
-        this.ratingService.sendRating(rating)
-    }
+    this.ratingRepository.save(rating);
+    this.ratingService.sendRating(rating)
+  }
 
+  @GetMapping("/ratings")
+  fun getRatings(): Iterable<Rating> {
+    LOGGER.debug("Get all ratings...");
+    return this.ratingRepository.findAll()
+  }
 }
