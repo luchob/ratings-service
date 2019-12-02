0ec96da4514d1450a880774532a6ec6bf275aca5
georgi.peychev <georgi.peychev@rewe-digital.com>
2019-12-02 19:53:07 +0200

    removed web sockets.

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,6 @@ extra.apply {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("org.springframework.boot:spring-boot-starter-websocket")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
 	implementation("org.liquibase:liquibase-core")
--- src/main/kotlin/com/java2days/ratings/web/RatingsController.kt
+++ src/main/kotlin/com/java2days/ratings/web/RatingsController.kt
@@ -2,15 +2,13 @@ package com.java2days.ratings.web
 
 import com.java2days.ratings.model.Rating
 import com.java2days.ratings.repository.RatingRepository
-import com.java2days.ratings.service.RatingsService
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @CrossOrigin(origins = ["*"])
 @RestController
-class RatingsController(val ratingRepository: RatingRepository,
-                        val ratingService: RatingsService) {
+class RatingsController(val ratingRepository: RatingRepository) {
 
   companion object {
     private val LOGGER = LoggerFactory.getLogger(RatingsController::class.java)
@@ -22,7 +20,6 @@ class RatingsController(val ratingRepository: RatingRepository,
     LOGGER.debug("Received a rating, trying to save...");
 
     this.ratingRepository.save(rating);
-    this.ratingService.sendRating(rating)
   }
 
   @GetMapping("/ratings/{lastRatingId}")
